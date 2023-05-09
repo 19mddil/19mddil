ee4b8faf47182fad8a3643b3fe59051514c88407
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-05-09 10:11:01 +0600

    more house keeping

--- src/components/FavBooks.js
+++ src/components/FavBooks.js
@@ -8,7 +8,7 @@ const FavBooks = () => (
             <div>
                 <div class="card-group" style={{ color: 'black' }}>
                     <div class="card">
-                        <img class="card-img-top" src="./assets/img/js.png" alt="Card image cap" />
+                        <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/js.png" alt="Card image cap" />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -18,7 +18,7 @@ const FavBooks = () => (
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/db.png" alt="Card image cap" />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -28,7 +28,7 @@ const FavBooks = () => (
                         </div>
                     </div>
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c.png" alt="Card image cap" />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -39,7 +39,7 @@ const FavBooks = () => (
                     </div>
 
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c%2B%2B.png" alt="Card image cap" />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -52,7 +52,7 @@ const FavBooks = () => (
                 </div>
                 <div class="card-group" style={{ color: 'black' }}>
                     <div class="card">
-                        <img class="card-img-top" src="..." alt="Card image cap" />
+                        <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/java.png" alt="Card image cap" />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
