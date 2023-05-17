5091c53d3b2dd61a268a4cb5ed9b5c634b899d43
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-05-17 12:28:15 +0600

    deploy ready

--- src/components/FavBooks.js
+++ src/components/FavBooks.js
@@ -5,74 +5,46 @@ import { CardGroup, Card, CardTitle, CardSubtitle, CardText, CardImg, CardBody }
 const FavBooks = () => (
     <div>
         <Layout title='FavBooks' className='container'>
-            <div>
+            {/* <div  >
                 <div class="card-group" style={{ color: 'black' }}>
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/js.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/db.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
 
                 </div>
                 <div class="card-group" style={{ color: 'black' }}>
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/java.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/linux.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
 
 
-                    <div class="card">
+                    <div class="card" style={{ width: "80%", height: "80%" }}>
                         <img class="card-img-top" src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c%2B%2B.png" alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
-                        </div>
+
                     </div>
                 </div>
+            </div> */}
+            <div >
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/js.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/db.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/java.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/linux.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
+                <img src="https://raw.githubusercontent.com/19mddil/19mddil/main/public/assets/img/c%2B%2B.png" alt="Card image cap" style={{ display: 'inline-block', width: "30%", height: "30%" }} />
             </div>
         </Layout >
     </div >
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,108 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem,
+    List
+} from 'reactstrap';
 import Layout from './Layout';
 
-const Projects = () => (
-    <div>
-        <Layout title='Projects' className='container'>
-        </Layout>
-    </div>
-)
+const Projects = props => {
+    const [open, setOpen] = useState('');
+    const toggle = (id) => {
+        if (open === id) {
+            setOpen();
+        } else {
+            setOpen(id);
+        }
+    };
+
+    return (
+        <div>
+            <Layout title='My Projects' className='container'>
+                <p>Currently I am working on the following projects which are under development.</p>
+                <Accordion open={open} toggle={toggle}>
+                    <AccordionItem>
+                        <AccordionHeader targetId="1">www.freelancebangla.com</AccordionHeader>
+                        <AccordionBody accordionId="1">
+                            <h3>This is my Entrepreneurship Project</h3>
+                            <p><i>To view the source code:</i> <a href='https://github.com/19mddil/FREELANCEBANGLA-MERN-LOCAL'>https://github.com/19mddil/FREELANCEBANGLA-MERN-LOCAL</a> </p>
+                            <p><i>To visit the webpage:</i> <a href='https://www.freelancebangla.com'>https://www.freelancebangla.com</a> </p>
+                            <p><i>The ghpages server:</i> <a href='https://19mddil.github.io/freelancebangla/'>https://19mddil.github.io/freelancebangla/</a></p>
+                            <hr></hr>
+                            The following features are under development
+                            <List>
+                                <li>
+                                    Clients homepage to show filtered workers.
+                                </li>
+                                <li>
+                                    Workers homepage to show filtered works.
+                                </li>
+                                <li>
+                                    Notifications on many events
+                                </li>
+                                <li>
+                                    Details of profiles
+                                </li>
+                                <li>
+                                    Payment
+                                </li>
+                                <li>
+                                    Time management and calculation for applications
+                                </li>
+                                <li>
+                                    Email varification
+                                </li>
+                            </List>
+                        </AccordionBody>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionHeader targetId="2">19mddil.github.io/ecommerce</AccordionHeader>
+                        <AccordionBody accordionId="2">
+                            <h3>This is a MERN project</h3>
+                            <p><i>To view the source code:</i> <a href='https://github.com/19mddil/ECOMMERCE-MERN-LOCAL'>https://github.com/19mddil/ECOMMERCE-MERN-LOCAL</a> </p>
+                            <p><i>To visit the webpage:</i> <a href='https://19mddil.github.io/ecommerce/'>https://19mddil.github.io/ecommerce/</a> </p>
+                            <hr></hr>
+                            The following features are under development
+                            <List>
+                                <li>
+                                    Customers homepage to show filtered products.
+                                </li>
+                                <li>
+                                    Notifications on many events
+                                </li>
+                                <li>
+                                    Details of profiles
+                                </li>
+                                <li>
+                                    Payment
+                                </li>
+                                <li>
+                                    Email varification
+                                </li>
+                            </List>
+                        </AccordionBody>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionHeader targetId="3">19mddil.github.io/19mddil</AccordionHeader>
+                        <AccordionBody accordionId="3">
+                            <h3>This is a portfolio project</h3>
+                            <p><i>To view the source code:</i> <a href='https://github.com/19mddil/19mddil'>https://github.com/19mddil/19mddil</a> </p>
+                            <p><i>To visit the webpage:</i> <a href='https://19mddil.github.io/19mddil/'>https://19mddil.github.io/19mddil/</a> </p>
+                            <hr></hr>
+                            The following features are under development
+                            <List>
+                                <li>
+                                    My many certificats are to be added.
+                                </li>
+                            </List>
+                        </AccordionBody>
+                    </AccordionItem>
+                </Accordion>
+            </Layout>
+        </div>
+    );
+}
 
 export default Projects;
